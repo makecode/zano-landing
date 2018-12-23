6e4b54f677617d3f8580a44e680f97ee7eb0a213
Anton Chupak <tony.chupak@gmail.com>
2018-12-23 19:42:54 +0200

    updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
 $(document).ready(function () {
-  //
+  // const $dropDownButtons = $('.drop-down__toggle');
+
+  // $dropDownButtons.map((index, btn) => {
+  //   $(btn).click(() => $(btn).next().toggleClass('show'));
+  // });
 });
