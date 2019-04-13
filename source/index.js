7638fb41daf7a0068fb64cda23a10394ff99bfe9
Anton Chupak <tony.chupak@gmail.com>
2019-04-13 21:20:39 +0800

    Animation - animation | v1 - update links

--- source/index.js
+++ source/index.js
@@ -43,12 +43,8 @@ tlHero
 // section CORE
 // prepare positions of elements
 TweenMax.set('.section.core .section__wrap', { y: 200, autoAlpha: 0, transformOrigin: 'center' });
-TweenMax.set('#icon-core #_x31_7', { y: 160, scale: 0.7, autoAlpha: 0, transformOrigin: 'center' });
-TweenMax.set('#icon-core #_x31_3', { y: 160, scale: 0.7, autoAlpha: 0, transformOrigin: 'center' });
-TweenMax.set('#icon-core #_x33__1_', { y: 160, scale: 0.7, autoAlpha: 0, transformOrigin: 'center' });
-TweenMax.set('#icon-core #_x34_6', { autoAlpha: 0, scale: 0.5, transformOrigin: 'center' });
-TweenMax.set('#icon-core #_x34_0', { autoAlpha: 0, scale: 0.5, transformOrigin: 'center' });
-TweenMax.set('#icon-core #_x33_4', { autoAlpha: 0, scale: 0.5, transformOrigin: 'center' });
+TweenMax.set('#icon-core #_x31_7, #icon-core #_x31_3, #icon-core #_x33__1_', { y: 160, scale: 0.7, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set('#icon-core #_x34_6, #icon-core #_x34_0, #icon-core #_x33_4', { autoAlpha: 0, scale: 0.5, transformOrigin: 'center' });
 
 const tlCore = new TimelineMax();
 const tlCoreSection = new TimelineMax();
@@ -81,7 +77,19 @@ TweenMax.set('.section.privacy .section__wrap', { y: 400, autoAlpha: 0 });
 TweenMax.set('.section.privacy .section__icon', { scale: 0.4, autoAlpha: 0 });
 
 const privacyItems1 = '#icon-privacy #_x34_03, #icon-privacy #_x33_98, #icon-privacy #_x33_93, #icon-privacy #_x33_88, #icon-privacy #_x33_83';
+const privacyItems2 = '#icon-privacy #_x33_86, #icon-privacy #_x33_91, #icon-privacy #_x33_96, #icon-privacy #_x34_01, #icon-privacy #_x34_06';
+const privacyItems3 = '#icon-privacy #_x33_65, #icon-privacy #_x33_63, #icon-privacy #_x33_62, #icon-privacy #_x33_60, #icon-privacy #_x33_59, #icon-privacy #_x33_57, #icon-privacy #_x33_56, #icon-privacy #_x33_54, #icon-privacy #_x33_51, #icon-privacy #_x33_53';
 TweenMax.set(privacyItems1, { scale: 0, transformOrigin: 'center' });
+TweenMax.set(privacyItems2, { scale: 0.4, y: 150, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set(privacyItems3, { scale: 0.3, y: 50, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set('#icon-privacy #_x33_67', { x: -50, y: -24, scale: 0.3, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set('#icon-privacy #_x33_70', { x: -50, y: 24, scale: 0.3, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set('#icon-privacy #_x33_73', { y: 50, scale: 0.3, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set('#icon-privacy #_x33_76', { x: 50, y: 24, scale: 0.3, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set('#icon-privacy #_x33_79', { x: 50, y: -24, scale: 0.3, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set('#icon-privacy #_x34_13', { y: 60, transformOrigin: 'center' });
+TweenMax.set('#icon-privacy #_x34_10', { y: 110, transformOrigin: 'center' });
+TweenMax.set('#icon-privacy #_x33_41', { y: 160, scale: 0.3, autoAlpha: 0, transformOrigin: 'center' });
 
 const tlPrivacy = new TimelineMax();
 const tlPrivacyText = new TimelineMax();
@@ -91,8 +99,13 @@ tlPrivacyText
   .to('.section.privacy .section__wrap', 2, { y: 0, autoAlpha: 1 });
 
 tlPrivacyIcon
-  .to('.section.privacy .section__icon', 2, { scale: 1, autoAlpha: 1})
-  .to(privacyItems1, 2, { scale: 1});
+  .to('.section.privacy .section__icon', 2, { scale: 1, autoAlpha: 1 })
+  .to(privacyItems1, 0.5, { scale: 1 })
+  .to(privacyItems2, 0.5, { scale: 1, y: 0, autoAlpha: 1 }, '-=1')
+  .to('#icon-privacy #_x33_67, #icon-privacy #_x33_70, #icon-privacy #_x33_73, #icon-privacy #_x33_76, #icon-privacy #_x33_79', 1, { x: 0, y: 0, scale: 1, autoAlpha: 1 })
+  .to(privacyItems3, 0.5, { scale: 1, y: 0, autoAlpha: 1 })
+  .to('#icon-privacy #_x34_13, #icon-privacy #_x34_10', 0.5, { y: 0 })
+  .to('#icon-privacy #_x33_41', 0.5, { scale: 1, y: 0, autoAlpha: 1 }, '-=0.5')
 
 tlPrivacy
   .add([tlPrivacyText, tlPrivacyIcon]);
@@ -112,16 +125,26 @@ const scenePrivacy = new ScrollMagic.Scene({
 // prepare positions of elements
 TweenMax.set('.section.ecommerce .features', { y: 200, autoAlpha: 0 });
 TweenMax.set('.section.ecommerce .section__icon', { scale: 0.4, autoAlpha: 0 });
+TweenMax.set('#icon-ecommerce #_x33_25', { autoAlpha: 0 });
+TweenMax.set('#icon-ecommerce #_x32_81, #icon-ecommerce #_x32_90, #icon-ecommerce #_x32_99, #icon-ecommerce #_x32_52', { autoAlpha: 0 });
+TweenMax.set('#icon-ecommerce #_x32_27, #icon-ecommerce #_x32_13, #icon-ecommerce #_x32_03, #icon-ecommerce #_x31_93, #icon-ecommerce #_x31_88', { y: 100, scale: 0.3, autoAlpha: 0, transformOrigin: 'center' });
+TweenMax.set('#icon-ecommerce #_x32_31, #icon-ecommerce #_x32_07, #icon-ecommerce #_x32_21, #icon-ecommerce #_x31_97, #icon-ecommerce #_x31_83', { y: -100, scaleY: 0, autoAlpha: 0, transformOrigin: 'bottom' });
+TweenMax.set('#icon-ecommerce #_x32_49, #icon-ecommerce #_x32_46, #icon-ecommerce #_x32_43, #icon-ecommerce #_x32_40, #icon-ecommerce #_x32_37', { scale: 0, transformOrigin: 'center' });
 
 const tlEcommerce = new TimelineMax();
 const tlEcommerceText = new TimelineMax();
 const tlEcommerceIcon = new TimelineMax();
 
 tlEcommerceIcon
-  .to('.section.ecommerce .section__icon', 3, { scale: 1, autoAlpha: 1 })
+  .to('.section.ecommerce .section__icon', 2, { scale: 1, autoAlpha: 1 })
+  .to('#icon-ecommerce #_x32_81, #icon-ecommerce #_x32_90, #icon-ecommerce #_x32_99', 0.5, { autoAlpha: 1 })
+  .to('#icon-ecommerce #_x32_52', 0.5, { autoAlpha: 1 })
+  .to('#icon-ecommerce #_x32_49, #icon-ecommerce #_x32_46, #icon-ecommerce #_x32_43, #icon-ecommerce #_x32_40, #icon-ecommerce #_x32_37', 0.5, { scale: 1 })
+  .to('#icon-ecommerce #_x32_27, #icon-ecommerce #_x32_13, #icon-ecommerce #_x32_03, #icon-ecommerce #_x31_93, #icon-ecommerce #_x31_88', 0.5, { y: 0, scale: 1, autoAlpha: 1 })
+  .to('#icon-ecommerce #_x32_31, #icon-ecommerce #_x32_07, #icon-ecommerce #_x32_21, #icon-ecommerce #_x31_97, #icon-ecommerce #_x31_83', 0.5, { y: 0, scaleY: 1, autoAlpha: 1 }, '-=0.5')
 
 tlEcommerceText
-  .to('.section.ecommerce .features', 3, { y: 0, autoAlpha: 1 });
+  .to('.section.ecommerce .features', 2, { y: 0, autoAlpha: 1 });
 
 tlEcommerce
   .add([tlEcommerceText, tlEcommerceIcon]);
@@ -152,7 +175,10 @@ tlWalletText
   .to('.section.wallet .section__left', 2, { autoAlpha: 1 });
 
 tlWalletIcon
-  .to('#icon-wallet .display-data, #icon-wallet .display-shadow-one, #icon-wallet .display-shadow-two', 2, { x: 0, y: 0 });
+  .to('#icon-wallet .display-data, #icon-wallet .display-shadow-one, #icon-wallet .display-shadow-two', 1, { x: 0, y: 0 })
+  .to('#icon-wallet .display-data', 2, { x: 113, y: 126 })
+  .to('#icon-wallet .display-shadow-one', 2, { x: 74, y: 74 }, '-=2')
+  .to('#icon-wallet .display-shadow-two', 2, { autoAlpha: 0 }, '-=4');
 
 tlWallet
   .add([tlWalletIcon, tlWalletText]);
